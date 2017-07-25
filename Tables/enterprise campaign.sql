7260a10e775505a6a2b666137aaff03d910d1023
Michał Bluj <m.bluj@polsource.com>
2017-07-25 13:25:51 +0200

    enterprise tables

--- Tables/enterprise campaign.sql
+++ Tables/enterprise campaign.sql
@@ -1,5 +1,4 @@
 create table caesars.enterprise_campaign (
-
 	id BIGSERIAL Primary Key,
 	i_dmid varchar(12),
 	i_pdb_trips_all_mkts INTEGER,
@@ -15,5 +14,23 @@ create table caesars.enterprise_campaign (
 	f_mdw_rated_all_mkts NUMERIC,
 	f_mdw_all_all_mkts NUMERIC,
 	contact varchar(18)
+)
 
+
+create table caesars.enterprise_campaign_12 (
+	id BIGSERIAL Primary Key,
+	i_dmid varchar(12),
+	i_pdb_trips_all_mkts INTEGER,
+	i_pdb_rated_trips_all_mkts  INTEGER,
+	i_cms_rated_trips_all_mkts  INTEGER,
+	i_all_days_all_mkts  INTEGER,
+	i_rated_days_all_mkts  INTEGER,
+	f_theo_all_all_mkts NUMERIC,
+	f_actual_all_all_mkts NUMERIC,
+	i_hotel_trips_all_mkts INTEGER,
+	i_offer_trips_all_mkts INTEGER,
+	i_minutes_all_mkts INTEGER,
+	f_mdw_rated_all_mkts NUMERIC,
+	f_mdw_all_all_mkts NUMERIC,
+	contact varchar(18)
 )
